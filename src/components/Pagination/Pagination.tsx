1fd00f2f161878770ac314b1dd1dbb75227c1dc3
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-06-12 22:31:09 -0400

    Update Pagination.tsx

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,31 +1,28 @@
-// src/components/Pagination/Pagination.tsx
-import ReactPaginate from 'react-paginate';
-import css from './Pagination.module.css';
+import ReactPaginate from "react-paginate";
+import css from "./Pagination.module.css";
 
-interface Props {
-  totalPages: number;
+interface PaginationProps {
   currentPage: number;
-  onPageChange: (page: number) => void;
+  pageCount: number;
+  onPageChange: (selected: number) => void;
 }
 
-const Pagination = ({ totalPages, currentPage, onPageChange }: Props) => {
+export default function Pagination({
+  currentPage,
+  pageCount,
+  onPageChange,
+}: PaginationProps) {
   return (
-      <ReactPaginate
-      breakLabel="..."
-      nextLabel="→"
-      previousLabel="←"
-      pageRangeDisplayed={3}
-      pageCount={totalPages} 
+    <ReactPaginate
+      pageCount={pageCount}
+      pageRangeDisplayed={5}
+      marginPagesDisplayed={1}
+      onPageChange={({ selected }) => onPageChange(selected + 1)}
       forcePage={currentPage - 1}
-      onPageChange={(event) => onPageChange(event.selected + 1)}
       containerClassName={css.pagination}
       activeClassName={css.active}
-      pageClassName={css.pageItem}
-      previousClassName={css.pageItem}
-      nextClassName={css.pageItem}
-      breakClassName={css.pageItem}
+      nextLabel="→"
+      previousLabel="←"
     />
   );
-};
-
-export default Pagination;
+}
